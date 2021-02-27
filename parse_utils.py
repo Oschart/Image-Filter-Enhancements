e3dc7937db1f0ab01b498f404b915f5c03c51f50
M.Ghanem <mohamed.ghanem@nabtahealth.com>
2021-02-27 19:07:07 +0200

    Added parsing utils

---
+++ parse_utils.py
@@ -0,0 +1,24 @@
+#%%
+import re
+
+trans_format = '''
+Please insert a list of your transformations in the following format:
+...<trans_key ...args>
+Available transormations:
+<TRANS offset>
+<SCALE factor>
+<ROT angle Px Py>
+<NTHP n>
+<HE>
+'''
+
+def parse_trans(trans_str):
+    ptrans = re.findall(r'\<(.*?)\>', trans_str)
+    sep_trans = []
+    for trans in ptrans:
+        sp = trans.split(' ')
+        sep_trans.append([sp[0], [float(st) for st in sp[1:]]])
+    return sep_trans
+
+#print(parse_trans("<TRANS ssss sss> <SCALE >"))
+# %%
