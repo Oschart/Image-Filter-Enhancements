8a9191fe919dced87d246d0ecec7b2b1521fee63
M.Ghanem <mohamed.ghanem@nabtahealth.com>
2021-03-06 02:28:32 +0200

    Refactoring display code

--- parse_utils.py
+++ parse_utils.py
@@ -1,13 +1,16 @@
 #%%
 import re
+import matplotlib.pyplot as plt
+
 
 trans_format = '''
 Please insert a list of your transformations in the following format:
-...<trans_key ...args>
+<trans_key1 ...args1> <trans_key2 ...args2> ... <trans_key_n ...args_n> 
+
 Available transormations:
 <TRANS offset>
 <SCALE Sx Sy>
-<ROT angle Px Py>
+<ROT angle(degrees) Px Py>
 <NTHP n>
 <HE>
 '''
@@ -20,5 +23,37 @@ def parse_trans(trans_str):
         sep_trans.append([sp[0], [float(st) for st in sp[1:]]])
     return sep_trans
 
-#print(parse_trans("<TRANS ssss sss> <SCALE >"))
+
+def display_single_filter(img_orig, img_filt, effect_name=None, aspect='equal', grad=None):
+    if grad is None:
+        n = 2
+        w = 5
+    else:
+        n = 3
+        w = 8
+    fig, axes = plt.subplots(
+        1, n, sharex=True, sharey=True, figsize=(8, w), dpi=100)
+    #fig.suptitle(filter_name)
+    axes[0].set_title('Original',
+                      fontdict=None, loc='center', color="k")
+    axes[0].imshow(img_orig, cmap='gray', aspect=aspect)
+    axes[0].get_xaxis().set_visible(False)
+    axes[0].get_yaxis().set_visible(False)
+
+    axes[1].set_title(effect_name,
+                      fontdict=None, loc='center', color="k")
+    axes[1].imshow(img_filt, cmap='gray', aspect=aspect)
+    axes[1].get_xaxis().set_visible(False)
+    axes[1].get_yaxis().set_visible(False)
+
+    if n == 3:
+        axes[2].set_title('Gradient',
+                          fontdict=None, loc='center', color="k")
+        axes[2].imshow(grad, cmap='gray', aspect=aspect)
+        axes[2].get_xaxis().set_visible(False)
+        axes[2].get_yaxis().set_visible(False)
+
+    for ax in axes:
+        ax.label_outer()
+    plt.show()
 # %%
