8f7f0d8e6bb7e4a396dd0574de279d597df248e1
M.Ghanem <mohamed.ghanem@nabtahealth.com>
2021-03-06 02:29:02 +0200

    Part 1 encapsulation

--- part1.py
+++ part1.py
@@ -2,30 +2,38 @@
 import numpy as np
 #import cv2
 from skimage.io import imread, imshow
-from parse_utils import trans_format
+from parse_utils import trans_format, parse_trans, display_single_filter
 from parse_utils import parse_trans
 from ImageTransformation import ImageTransformation
 from ImageTransformer import ImageTransformer
+import matplotlib.pyplot as plt
 
 
 
-#img_path = input(prompt="Insert image path")
-img_path = 'he.jpg'
-img = imread(img_path, as_gray=True)
-#trans_str = input(prompt=trans_format)
-trans_str = '''
-<HE>
-'''
-ptrans = parse_trans(trans_str)
-trans_list = []
-for ptr in ptrans:
-    trans = ImageTransformation(key=ptr[0], args=ptr[1])
-    trans_list.append(trans)
+def run(interactive=True, img_path='samples/rome.jpg', trans_str='', effect='', aspect='equal',side_by_side=True):
+    if interactive:
+        img_path = input("Insert image path")
+        trans_str = input(trans_format)
+
+    img = imread(img_path, as_gray=True)
+
+    ptrans = parse_trans(trans_str)
+    trans_list = []
+    for ptr in ptrans:
+        trans = ImageTransformation(key=ptr[0], args=ptr[1])
+        trans_list.append(trans)
 
 
-transformer = ImageTransformer(tr_list=trans_list)
+    transformer = ImageTransformer(tr_list=trans_list)
 
-output = transformer.apply_all(img)
-imshow(output)
+    output = transformer.apply_all(img)
+    if side_by_side:
+        display_single_filter(img, output, effect_name=effect, aspect=aspect)
+    else:
+        plt.imshow(img, cmap='gray')
+        plt.title('Original Image')
+        plt.show()
+        plt.imshow(output, cmap='gray')
+        plt.title(effect)
+        plt.show()
 
-# %%
